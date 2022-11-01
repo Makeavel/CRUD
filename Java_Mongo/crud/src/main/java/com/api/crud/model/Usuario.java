82926259026268d9622f0eb9c5aea595aeb1ce8a
Paulo <dpseutroco@gmail.com>
2022-11-01 21:29:23 +0000

    spring_mongo

---
+++ Java_Mongo/crud/src/main/java/com/api/crud/model/Usuario.java
@@ -0,0 +1,12 @@
+package com.api.crud.model;
+
+@Data
+@Document(collections = "Usuario")
+public class Usuario {
+
+    private String nome;
+    private String cpf;
+    
+
+
+}
---
+++ Java_Mongo/crud/src/main/java/com/api/crud/repository/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.api.crud.repository;
+
+@Repository
+public interface UsuarioRepository extends MongoRepository<Usuario , String>{
+    
+}

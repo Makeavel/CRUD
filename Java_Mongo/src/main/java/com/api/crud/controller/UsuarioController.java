d61b812a48e1f8e22b8b05558a6bb07672090c15
Paulo <dpseutroco@gmail.com>
2023-02-02 08:33:07 -0300

    Update UsuarioController.java

--- Java_Mongo/src/main/java/com/api/crud/controller/UsuarioController.java
+++ Java_Mongo/src/main/java/com/api/crud/controller/UsuarioController.java
@@ -7,12 +7,23 @@ public class UsuarioController{
     @Autowired
     private UsuarioService usuarioService;
 
-    @GetMapping("/create/{nome}")
+    @PostMapping("/create/{nome}")
     public Usuario getCreate(@PathVariable("nome")String nome){
         return usuarioService.findGetCreate(nome);
     }
 
+    @GetMapping("/read")
+    public Usuario getReadAll(){
+        return usuarioService.findGetReadAll();
+    }
     
-
+    @PutMapping("/update/{cpf}")
+    public Usuario getUpdate(@PathVariable("cpf")String cpf){
+        return usuarioService.findPutUser(cpf);
+        
+    @DeleteMapping("/delete/{cpf}")
+    public Usuairo getDelete(@PathVariable("cpf)String cpf){
+        return usuarioService.findDeleteUser(cpf);
+    
 
 }
